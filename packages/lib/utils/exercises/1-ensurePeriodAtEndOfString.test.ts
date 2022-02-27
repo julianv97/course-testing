ea292445a4120e89a70862b3071061160f065036
julianv97 <julian.vicente@radiumrocket.com>
2022-02-27 14:00:44 -0300

    Class 1: exercise 1 complete

---
+++ packages/lib/utils/exercises/1-ensurePeriodAtEndOfString.test.ts
@@ -0,0 +1,24 @@
+import * as Utils from './index';
+
+describe('ensurePeriodAtEndOfString', () => {
+  it('return the same if the input already has a .', () => {
+    const input = "Hi, I'm Lucía.";
+    const expected = "Hi, I'm Lucía.";
+    const actual = Utils.ensurePeriodAtEndOfString(input);
+    expect(actual).toEqual(expected);
+  });
+
+  it("should add a period to the end of the string if it doesn't already end with a period", () => {
+    const input = 'Hello';
+    const expected = 'Hello.';
+    const actual = Utils.ensurePeriodAtEndOfString(input);
+    expect(actual).toEqual(expected);
+  });
+
+  it('shoul return an empty string if the input is falsy', () => {
+    const input = '';
+    const expected = '';
+    const actual = Utils.ensurePeriodAtEndOfString(input);
+    expect(actual).toEqual(expected);
+  });
+});
